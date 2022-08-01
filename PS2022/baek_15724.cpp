82c3a6c96610d6df3d1bb91dedb1de4d751756d7
Junyoung, Jang <39786044+Amuguna@users.noreply.github.com>
2022-08-01 22:32:19 +0900

    Create baek_15724.cpp
    시간 초과

---
+++ PS2022/baek_15724.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1025][1025]={0,};
+int dp[1025][1025]={0,};
+int N, M, tmp, fr, fc, sr, sc;
+int main() {
+  cin >> N >> M;
+  
+  for(int i=1;i<=N;i++) {
+    for(int j=1;j<=M;j++) {
+      cin >> arr[i][j];
+      dp[i][j] = arr[i][j]+dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
+    }
+  }
+  cin >> tmp;
+  for(int k=0; k<tmp;k++) {
+    cin >> fr >> fc >> sr >> sc;
+    cout << dp[sr][sc]-dp[sr][fc-1]-dp[fr-1][sc]+dp[fr-1][fc-1] << endl;
+  }
+}

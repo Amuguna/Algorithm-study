ed34309cd2c64d1980a9fcd60f62f69d17c5a1d9
Junyoung, Jang <39786044+Amuguna@users.noreply.github.com>
2022-08-02 00:47:43 +0900

    Update baek_15724.cpp

--- PS2022/baek_15724.cpp
+++ PS2022/baek_15724.cpp
@@ -5,8 +5,10 @@ int arr[1025][1025]={0,};
 int dp[1025][1025]={0,};
 int N, M, tmp, fr, fc, sr, sc;
 int main() {
+  ios_base::sync_with_stdio(false); 
+  cin.tie(NULL); 
+  cout.tie(NULL);
   cin >> N >> M;
-  
   for(int i=1;i<=N;i++) {
     for(int j=1;j<=M;j++) {
       cin >> arr[i][j];
@@ -16,6 +18,6 @@ int main() {
   cin >> tmp;
   for(int k=0; k<tmp;k++) {
     cin >> fr >> fc >> sr >> sc;
-    cout << dp[sr][sc]-dp[sr][fc-1]-dp[fr-1][sc]+dp[fr-1][fc-1] << endl;
+    printf("%d\n", dp[sr][sc]-dp[sr][fc-1]-dp[fr-1][sc]+dp[fr-1][fc-1]);
   }
 }

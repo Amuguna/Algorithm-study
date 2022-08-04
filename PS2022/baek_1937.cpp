d3a9887f5394770f8de46ec97d085af9ef57d469
Junyoung, Jang <39786044+Amuguna@users.noreply.github.com>
2022-08-04 21:30:52 +0900

    Update baek_1937.cpp

--- PS2022/baek_1937.cpp
+++ PS2022/baek_1937.cpp
@@ -1,56 +1,62 @@
+/*
+404kb
+32ms
+*/
 #include <iostream>
-#include <queue>
+#define MAX 500
 using namespace std;
-int n, r, c, ans=1;
+
+int arr[MAX][MAX];
+int DP[MAX][MAX] = {0,};
+int n, ans=0;
 int dir_c[4] = {0, 0, -1, 1};
 int dir_r[4] = {-1, 1, 0, 0};
-pair<pair<int, int>, int> p, ps;
-queue<pair<pair<int, int>, int>> q;
+
+void input() {
+  cin >> n;
+  for(int i=0;i<n;i++) {
+    for(int j=0;j<n;j++) {
+      cin >> arr[i][j];
+    }
+  }
+}
 bool in_size(int a, int b) {
   if(a >= 0 && a < n && b >= 0 && b < n)
     return true;
   else
     return false;
 }
+int move(int r, int c) {
+  if(DP[r][c] != 0)
+    return DP[r][c];
+  DP[r][c] = 1;
+  for(int i=0;i<4;i++) {
+    int nr = r + dir_r[i];
+    int nc = c + dir_c[i];
+    if(in_size(nr, nc)) {
+      if(arr[r][c] < arr[nr][nc]) {
+        int tmp = move(nr, nc)+1;
+        if(DP[r][c] < tmp) {
+          DP[r][c] = tmp;
+        }
+      }
+    }
+  }
+  return DP[r][c];
+}
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  //입력부
-  cin >> n;
-  int arr[n][n];
-  for(int i=0;i<n;i++) {
+  
+  input();
+
+  for(int i=0;i<n;i++)
     for(int j=0;j<n;j++) {
-      cin >> arr[i][j];
-    }
-  }
-  //logic
-  for(int k=0;k<n;k++) {
-    for(int j=0;j<n;j++) {
-      p.first.first = k;
-      p.first.second = j;
-      p.second = 1;
-      q.push(p);
-      
-      while(!q.empty()) {
-        p = q.front();
-        q.pop();
-        r = p.first.first;
-        c = p.first.second;
-        for(int i=0;i<4;i++) {
-          if(!in_size(r + dir_r[i], c + dir_c[i]))
-            continue;
-          if(arr[r][c] < arr[r + dir_r[i]][c + dir_c[i]]) {
-            ps.first.first = r + dir_r[i];
-            ps.first.second = c + dir_c[i];
-            ps.second = p.second + 1;
-            q.push(ps);
-          }
-        }
+      int mv_tmp = move(i, j);
+      if(ans < mv_tmp) {
+        ans = mv_tmp;
       }
-      if(ans < p.second)
-        ans = p.second;  
-    }
-  }
-  cout << ans;
+    }  
+  cout << ans << '\n';
 }

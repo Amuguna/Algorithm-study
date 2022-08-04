31dfbea3f9aedc678876698b246ff576ee682cfb
Junyoung, Jang <39786044+Amuguna@users.noreply.github.com>
2022-08-04 20:30:44 +0900

    Update baek_1937.cpp

--- PS2022/baek_1937.cpp
+++ PS2022/baek_1937.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-int n, r, c;
+int n, r, c, ans=1;
 int dir_c[4] = {0, 0, -1, 1};
 int dir_r[4] = {-1, 1, 0, 0};
 pair<pair<int, int>, int> p, ps;
@@ -22,28 +22,35 @@ int main() {
   for(int i=0;i<n;i++) {
     for(int j=0;j<n;j++) {
       cin >> arr[i][j];
-      p.first.first = i;
-      p.first.second = j;
-      p.second = 1;
-      q.push(p);
     }
   }
   //logic
-  while(!q.empty()) {
-    p = q.front();
-    q.pop();
-    r = p.first.first;
-    c = p.first.second;
-    for(int i=0;i<4;i++) {
-      if(!in_size(r + dir_r[i], c + dir_c[i]))
-        continue;
-      if(arr[r][c] < arr[r + dir_r[i]][c + dir_c[i]]) {
-        ps.first.first = r + dir_r[i];
-        ps.first.second = c + dir_c[i];
-        ps.second = p.second + 1;
-        q.push(ps);
+  for(int k=0;k<n;k++) {
+    for(int j=0;j<n;j++) {
+      p.first.first = k;
+      p.first.second = j;
+      p.second = 1;
+      q.push(p);
+      
+      while(!q.empty()) {
+        p = q.front();
+        q.pop();
+        r = p.first.first;
+        c = p.first.second;
+        for(int i=0;i<4;i++) {
+          if(!in_size(r + dir_r[i], c + dir_c[i]))
+            continue;
+          if(arr[r][c] < arr[r + dir_r[i]][c + dir_c[i]]) {
+            ps.first.first = r + dir_r[i];
+            ps.first.second = c + dir_c[i];
+            ps.second = p.second + 1;
+            q.push(ps);
+          }
+        }
       }
+      if(ans < p.second)
+        ans = p.second;  
     }
   }
-  cout << p.second;
+  cout << ans;
 }
